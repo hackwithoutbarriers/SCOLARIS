fd42b1818835d1743b3583164b87c55df8220bc7
Remy A. Agbokpe <205567368+hackwithoutbarriers@users.noreply.github.com>
2026-09-09 09:43:09 +0000

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ set -eu
 port="${PORT:-10000}"
 sed -i "s/Listen 80/Listen ${port}/; s#<VirtualHost \\*:80>#<VirtualHost *:${port}>#" /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
 
+php artisan optimize:clear
 php artisan storage:link --force
 php artisan migrate --force
 php artisan optimize
