994c69718175d8ce4c1ffc7af9f8e716f725214d
Remy A. Agbokpe <205567368+hackwithoutbarriers@users.noreply.github.com>
2026-09-09 08:15:54 +0000

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,12 @@ port="${PORT:-10000}"
 sed -i "s/Listen 80/Listen ${port}/; s#<VirtualHost \\*:80>#<VirtualHost *:${port}>#" /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
 
 php artisan storage:link --force
-php artisan optimize:clear
 php artisan migrate --force
 php artisan optimize
+
+if [ -n "${SCOLARIS_OWNER_EMAIL:-}" ] && [ -n "${SCOLARIS_OWNER_PASSWORD:-}" ]; then
+    php artisan scolaris:create-super-admin
+fi
 
 if [ "$#" -gt 0 ]; then
     exec "$@"
