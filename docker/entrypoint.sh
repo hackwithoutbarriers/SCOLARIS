f7bffd1f2cfbcc0c238831c6abb25ac7778ca67f
Remy A. Agbokpe <205567368+hackwithoutbarriers@users.noreply.github.com>
2026-09-09 01:50:47 +0000

    Add files via upload

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,7 @@ port="${PORT:-10000}"
 sed -i "s/Listen 80/Listen ${port}/; s#<VirtualHost \\*:80>#<VirtualHost *:${port}>#" /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
 
 php artisan storage:link --force
+php artisan optimize:clear
 php artisan migrate --force
 php artisan optimize
 
