9a29eefb4745259d82d88b204bc4f645ce09e970
Remy A. Agbokpe <205567368+hackwithoutbarriers@users.noreply.github.com>
2026-09-09 15:48:54 +0000

    Add files via upload

---
+++ app/Http/Middleware/SecurityHeaders.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityHeaders
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        if (app()->environment('production')) {
+            $response->headers->set(
+                'Strict-Transport-Security',
+                'max-age=31536000; includeSubDomains'
+            );
+        }
+
+        return $response;
+    }
+}
