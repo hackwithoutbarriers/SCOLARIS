c5172410c3fbd9a7f483d369f134efdc8c29e56a
Remy A. Agbokpe <205567368+hackwithoutbarriers@users.noreply.github.com>
2026-09-09 19:52:56 +0000

    Add files via upload

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->viteTheme('resources/css/app.css')
+            ->viteTheme('resources/css/filament/admin-theme.css')
             ->userMenuItems([
                 MenuItem::make()->label('Français')->url(url('/language/fr')),
             ])
