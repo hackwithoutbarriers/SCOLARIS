8aaa332ca63df6f3db2184b7893edf32b63a0e32
Remy A. Agbokpe <205567368+hackwithoutbarriers@users.noreply.github.com>
2026-09-09 07:43:13 +0000

    Create ListRegistrationRequests.php

---
+++ app/Filament/Resources/RegistrationRequestResource/Pages/ListRegistrationRequests.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\RegistrationRequestResource\Pages;
+
+use App\Filament\Resources\RegistrationRequestResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListRegistrationRequests extends ListRecords
+{
+    protected static string $resource = RegistrationRequestResource::class;
+}
